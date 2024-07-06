fecfd0bb630703caaadf319ba215e751cabb767f
phu <pminimalism2011@gmail.com>
2024-07-06 09:43:20 +0000

    update

--- train_zero.py
+++ train_zero.py
@@ -27,8 +27,22 @@ warnings.filterwarnings("ignore")
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda:0" if use_cuda else "cpu")
 
-CLASS_INDEX = {'Brain': 3, 'Liver': 2, 'Retina_RESC': 1, 'Retina_OCT2017': -1, 'Chest': -2, 'Histopathology': -3}  #
-CLASS_INDEX_INV = {3: 'Brain', 2: 'Liver', 1: 'Retina_RESC', -1: 'Retina_OCT2017', -2: 'Chest', -3: 'Histopathology'}  #
+CLASS_INDEX = {
+    "Brain": 3,
+    "Liver": 2,
+    "Retina_RESC": 1,
+    "Retina_OCT2017": -1,
+    "Chest": -2,
+    "Histopathology": -3,
+}  #
+CLASS_INDEX_INV = {
+    3: "Brain",
+    2: "Liver",
+    1: "Retina_RESC",
+    -1: "Retina_OCT2017",
+    -2: "Chest",
+    -3: "Histopathology",
+}  #
 
 
 def setup_seed(seed):
@@ -41,30 +55,52 @@ def setup_seed(seed):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Testing')
-    parser.add_argument('--model_name', type=str, default='ViT-L-14-336', help="ViT-B-16-plus-240, ViT-L-14-336")
-    parser.add_argument('--pretrain', type=str, default='openai', help="laion400m, openai")
-    parser.add_argument('--obj', type=str, default='Retina_RESC')
-    parser.add_argument('--data_path', type=str, default='./data/')
-    parser.add_argument('--ckpt_path', type=str, default='./ckpt/')
-    parser.add_argument('--batch_size', type=int, default=2)
-    parser.add_argument('--img_size', type=int, default=240)
+    parser = argparse.ArgumentParser(description="Testing")
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        default="ViT-L-14-336",
+        help="ViT-B-16-plus-240, ViT-L-14-336",
+    )
+    parser.add_argument(
+        "--pretrain", type=str, default="openai", help="laion400m, openai"
+    )
+    parser.add_argument("--obj", type=str, default="Retina_RESC")
+    parser.add_argument("--data_path", type=str, default="./data/")
+    parser.add_argument("--ckpt_path", type=str, default="./ckpt/")
+    parser.add_argument("--batch_size", type=int, default=2)
+    parser.add_argument("--img_size", type=int, default=240)
     parser.add_argument("--epoch", type=int, default=50, help="epochs")
-    parser.add_argument("--learning_rate", type=float, default=0.0001, help="learning rate")
-    parser.add_argument("--features_list", type=int, nargs="+", default=[6, 12, 18, 24], help="features used")
-    parser.add_argument('--seed', type=int, default=111)
+    parser.add_argument(
+        "--learning_rate", type=float, default=0.0001, help="learning rate"
+    )
+    parser.add_argument(
+        "--features_list",
+        type=int,
+        nargs="+",
+        default=[6, 12, 18, 24],
+        help="features used",
+    )
+    parser.add_argument("--seed", type=int, default=111)
     args = parser.parse_args()
 
     setup_seed(args.seed)
 
     # fixed feature extractor
-    clip_model = create_model(model_name=args.model_name, img_size=args.img_size, device=device,
-                              pretrained=args.pretrain, require_pretrained=True)
+    clip_model = create_model(
+        model_name=args.model_name,
+        img_size=args.img_size,
+        device=device,
+        pretrained=args.pretrain,
+        require_pretrained=True,
+    )
     clip_model.to(device)
     clip_model.eval()
     clip_model.visual.DAPM_replace(DPAM_layer=20)
 
-    model = CLIP_Inplanted(clip_model=clip_model, features=args.features_list).to(device)
+    model = CLIP_Inplanted(clip_model=clip_model, features=args.features_list).to(
+        device
+    )
     model.eval()
 
     for name, param in model.named_parameters():
@@ -76,24 +112,41 @@ def main():
         clip_model=clip_model,
         n_ctx=8,  # args.config.n_learnable_token
         CSC=True,  # args.config.CSC
-        class_token_position=['end'],  # args.config.class_token_positions
+        class_token_position=["end"],  # args.config.class_token_positions
     ).to(device)
 
     # optimizer for only adapters
-    prompt_optimizer = torch.optim.Adam(list(prompt_maker.prompt_learner.parameters()), lr=args.learning_rate,
-                                        betas=(0.5, 0.999))
-    seg_optimizer = torch.optim.Adam(list(model.seg_adapters.parameters()), lr=args.learning_rate, betas=(0.5, 0.999))
-    det_optimizer = torch.optim.Adam(list(model.det_adapters.parameters()), lr=args.learning_rate, betas=(0.5, 0.999))
+    prompt_optimizer = torch.optim.Adam(
+        list(prompt_maker.prompt_learner.parameters()),
+        lr=args.learning_rate,
+        betas=(0.5, 0.999),
+    )
+    seg_optimizer = torch.optim.Adam(
+        list(model.seg_adapters.parameters()), lr=args.learning_rate, betas=(0.5, 0.999)
+    )
+    det_optimizer = torch.optim.Adam(
+        list(model.det_adapters.parameters()), lr=args.learning_rate, betas=(0.5, 0.999)
+    )
 
     # load dataset and loader
-    kwargs = {'num_workers': 4, 'pin_memory': True} if use_cuda else {}
-    train_dataset = MedTrainDataset(args.data_path, args.obj, args.img_size, args.batch_size)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=True, **kwargs)
+    kwargs = {"num_workers": 4, "pin_memory": True} if use_cuda else {}
+    train_dataset = MedTrainDataset(
+        args.data_path, args.obj, args.img_size, args.batch_size
+    )
+    train_loader = torch.utils.data.DataLoader(
+        train_dataset, batch_size=1, shuffle=True, **kwargs
+    )
 
     test_dataset = MedTestDataset(args.data_path, args.obj, args.img_size)
-    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=1, shuffle=False, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        test_dataset, batch_size=1, shuffle=False, **kwargs
+    )
 
-    checkpoint = torch.load(os.path.join(f'/content/drive/MyDrive/Thesis/bubuchacha_2/zero-shot/Liver_7.pth'))
+    checkpoint = torch.load(
+        os.path.join(
+            f"/content/drive/MyDrive/Thesis/bubuchacha_2/zero-shot/Liver_7.pth"
+        )
+    )
     model.seg_adapters.load_state_dict(checkpoint["seg_adapters"])
     model.det_adapters.load_state_dict(checkpoint["det_adapters"])
     prompt_maker.prompt_learner.load_state_dict(checkpoint["prompt"])
@@ -115,27 +168,35 @@ def main():
     save_score = 0.0
     prompt_maker.train()
     for epoch in range(args.epoch):
-        print('epoch', epoch, ':')
+        print("epoch", epoch, ":")
         if epoch >= 0:
             score = test(args, model, test_loader, prompt_maker)
             if True:
                 save_score = score
-                ckp_path = f'{args.ckpt_path}/zero-shot/{args.obj}_{epoch}.pth'
-                torch.save({'seg_adapters': model.seg_adapters.state_dict(),
-                            'det_adapters': model.det_adapters.state_dict(),
-                            'prompt': prompt_maker.prompt_learner.state_dict()},
-                           ckp_path)
-                print(f'best epoch found: epoch {epoch}')
-            print('\n')
+                ckp_path = f"{args.ckpt_path}/zero-shot/{args.obj}_{epoch}.pth"
+                torch.save(
+                    {
+                        "seg_adapters": model.seg_adapters.state_dict(),
+                        "det_adapters": model.det_adapters.state_dict(),
+                        "prompt": prompt_maker.prompt_learner.state_dict(),
+                    },
+                    ckp_path,
+                )
+                print(f"best epoch found: epoch {epoch}")
+            print("\n")
 
         loss_list = []
-        for (image, image_label, mask, seg_idx) in tqdm(train_loader):
+        for image, image_label, mask, seg_idx in tqdm(train_loader):
 
             image = image.squeeze(0).to(device)
             seg_idx = seg_idx.item()
 
             with torch.cuda.amp.autocast():
-                _, [seg_patch_tokens, det_patch_tokens], [seg_patch_outs, det_patch_outs] = model(image)
+                (
+                    _,
+                    [seg_patch_tokens, det_patch_tokens],
+                    [seg_patch_outs, det_patch_outs],
+                ) = model(image)
                 seg_patch_tokens = [p[:, 1:, :] for p in seg_patch_tokens]
                 det_patch_tokens = [p[:, 1:, :] for p in det_patch_tokens]
 
@@ -145,9 +206,12 @@ def main():
                 det_loss = 0
                 image_label = image_label.squeeze(0).to(device)
                 for layer in range(len(det_patch_tokens)):
-                    det_patch_tokens[layer] = det_patch_tokens[layer] / det_patch_tokens[layer].norm(dim=-1,
-                                                                                                     keepdim=True)
-                    anomaly_map = (100.0 * det_patch_tokens[layer] @ prompt_adapter_features)
+                    det_patch_tokens[layer] = det_patch_tokens[
+                        layer
+                    ] / det_patch_tokens[layer].norm(dim=-1, keepdim=True)
+                    anomaly_map = (
+                        100.0 * det_patch_tokens[layer] @ prompt_adapter_features
+                    )
                     anomaly_map = torch.softmax(anomaly_map, dim=-1)[:, :, 1]
                     anomaly_score = torch.mean(anomaly_map, dim=-1)
                     det_loss += loss_bce(anomaly_score, image_label)
@@ -158,19 +222,28 @@ def main():
                     mask = mask.squeeze(0).to(device)
                     mask[mask > 0.5], mask[mask <= 0.5] = 1, 0
                     for layer in range(len(seg_patch_tokens)):
-                        seg_patch_tokens[layer] = seg_patch_tokens[layer] / seg_patch_tokens[layer].norm(dim=-1,
-                                                                                                         keepdim=True)
+                        seg_patch_tokens[layer] = seg_patch_tokens[
+                            layer
+                        ] / seg_patch_tokens[layer].norm(dim=-1, keepdim=True)
                         # print(seg_patch_tokens[layer].shape, text_feature_list[seg_idx].shape) # torch.Size([289, 768]) torch.Size([768, 2])
-                        anomaly_map = (100.0 * seg_patch_tokens[layer] @ prompt_adapter_features)
+                        anomaly_map = (
+                            100.0 * seg_patch_tokens[layer] @ prompt_adapter_features
+                        )
                         B, L, C = anomaly_map.shape
                         H = int(np.sqrt(L))
-                        anomaly_map = F.interpolate(anomaly_map.permute(0, 2, 1).view(B, 2, H, H),
-                                                    size=args.img_size, mode='bilinear', align_corners=True)
+                        anomaly_map = F.interpolate(
+                            anomaly_map.permute(0, 2, 1).view(B, 2, H, H),
+                            size=args.img_size,
+                            mode="bilinear",
+                            align_corners=True,
+                        )
                         anomaly_map = torch.softmax(anomaly_map, dim=1)
                         seg_loss += loss_focal(anomaly_map, mask)
                         seg_loss += loss_dice(anomaly_map[:, 1, :, :], mask)
 
-                    loss = seg_loss + det_loss  # = focal(seg_out, mask) + bce(det_out, y)
+                    loss = (
+                        seg_loss + det_loss
+                    )  # = focal(seg_out, mask) + bce(det_out, y)
                     loss.requires_grad_(True)
                     prompt_optimizer.zero_grad()
                     seg_optimizer.zero_grad()
@@ -192,7 +265,9 @@ def main():
                 loss_list.append(loss.item())
 
         train_dataset.shuffle_dataset()
-        train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=1, shuffle=True, **kwargs)
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset, batch_size=1, shuffle=True, **kwargs
+        )
 
         # logs
         print("Loss: ", np.mean(loss_list))
@@ -204,12 +279,16 @@ def test(args, seg_model, test_loader, prompt_maker):
     image_scores = []
     segment_scores = []
 
-    for (image, y, mask) in tqdm(test_loader):
+    for image, y, mask in tqdm(test_loader):
         image = image.to(device)
         mask[mask > 0.5], mask[mask <= 0.5] = 1, 0
 
         with torch.no_grad(), torch.cuda.amp.autocast():
-            _, [seg_patch_tokens, det_patch_tokens], [seg_patch_outs, det_patch_outs] = seg_model(image)
+            (
+                _,
+                [seg_patch_tokens, det_patch_tokens],
+                [seg_patch_outs, det_patch_outs],
+            ) = seg_model(image)
             seg_patch_tokens = [p[0, 1:, :] for p in seg_patch_tokens]
             det_patch_tokens = [p[0, 1:, :] for p in det_patch_tokens]
 
@@ -220,7 +299,9 @@ def test(args, seg_model, test_loader, prompt_maker):
             patch_tokens = det_patch_tokens.copy()
             for layer in range(len(patch_tokens)):
                 patch_tokens[layer] /= patch_tokens[layer].norm(dim=-1, keepdim=True)
-                anomaly_map = (100.0 * patch_tokens[layer] @ prompt_adapter_features).unsqueeze(0)
+                anomaly_map = (
+                    100.0 * patch_tokens[layer] @ prompt_adapter_features
+                ).unsqueeze(0)
                 anomaly_map = torch.softmax(anomaly_map, dim=-1)[:, :, 1]
                 anomaly_score += anomaly_map.mean()
             image_scores.append(anomaly_score.cpu())
@@ -230,11 +311,17 @@ def test(args, seg_model, test_loader, prompt_maker):
             anomaly_maps = []
             for layer in range(len(patch_tokens)):
                 patch_tokens[layer] /= patch_tokens[layer].norm(dim=-1, keepdim=True)
-                anomaly_map = (100.0 * patch_tokens[layer] @ prompt_adapter_features).unsqueeze(0)
+                anomaly_map = (
+                    100.0 * patch_tokens[layer] @ prompt_adapter_features
+                ).unsqueeze(0)
                 B, L, C = anomaly_map.shape
                 H = int(np.sqrt(L))
-                anomaly_map = F.interpolate(anomaly_map.permute(0, 2, 1).view(B, 2, H, H),
-                                            size=args.img_size, mode='bilinear', align_corners=True)
+                anomaly_map = F.interpolate(
+                    anomaly_map.permute(0, 2, 1).view(B, 2, H, H),
+                    size=args.img_size,
+                    mode="bilinear",
+                    align_corners=True,
+                )
                 anomaly_map = torch.softmax(anomaly_map, dim=1)[:, 1, :, :]
                 anomaly_maps.append(anomaly_map.cpu().numpy())
             final_score_map = np.sum(anomaly_maps, axis=0)
@@ -250,21 +337,23 @@ def test(args, seg_model, test_loader, prompt_maker):
     segment_scores = np.array(segment_scores)
     image_scores = np.array(image_scores)
 
-    segment_scores = (segment_scores - segment_scores.min()) / (segment_scores.max() - segment_scores.min())
-    image_scores = (image_scores - image_scores.min()) / (image_scores.max() - image_scores.min())
+    segment_scores = (segment_scores - segment_scores.min()) / (
+        segment_scores.max() - segment_scores.min()
+    )
+    image_scores = (image_scores - image_scores.min()) / (
+        image_scores.max() - image_scores.min()
+    )
 
     img_roc_auc_det = roc_auc_score(gt_list, image_scores)
-    print(f'{args.obj} AUC : {round(img_roc_auc_det, 4)}')
+    print(f"{args.obj} AUC : {round(img_roc_auc_det, 4)}")
 
     if CLASS_INDEX[args.obj] > 0:
         seg_roc_auc = roc_auc_score(gt_mask_list.flatten(), segment_scores.flatten())
-        print(f'{args.obj} pAUC : {round(seg_roc_auc, 4)}')
+        print(f"{args.obj} pAUC : {round(seg_roc_auc, 4)}")
         return seg_roc_auc + img_roc_auc_det
     else:
         return img_roc_auc_det
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
-
